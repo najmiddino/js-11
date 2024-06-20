0a3456009ea72d1b4ebe842c5210a1c7f65cedcd
najmiddino <164999098+najmiddino@users.noreply.github.com>
2024-06-20 13:22:26 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+// let a = +prompt('bitta son kiriting')
+// let b = +prompt('ikkinchi sonni kiriting')
+
+// if (a + b) {
+//     alert ( a + b + 8 ,`=`)
+// } 
+//2
+// let a = +('12')
+// console.log(a);
+
+
+//3
+
+
+// let randpm_number = Math.floor(Math.random()*100 )
+
+// if (randpm_number){
+//     randpm_number.push(randpm_number[i])
+// }
+
+
+// console.log(randpm_number);  ?
+
+
+//4
+
+// let a =prompt('sozkiriting')
+
+// if (a == 'mars'){
+//     alert(`mars`)
+// } else {
+//     alert(`mars it school`)
+// }
+
+//5
+
+// let arr = ['bmw','olma','sigir','buqaa','burun']
+
+// for()
+
+
+//6
+
+
+// let a = []
+// for(let i=0; i < 100; i++) {
+//     if([i] % 2 == 0){
+//         a.push([i])
+//     }
+// }
+// console.log(a, 'juft sonlari');
+
